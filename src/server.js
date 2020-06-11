4591df83c2e5ec44682bbebf481609f7718232b3
joaopd <40632469+joaopd@users.noreply.github.com>
2020-06-11 17:45:55 -0300

    configurações do servidor

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 const express = require("express")
 const server = express()
 
+//configurar caminhos da aplicação
+//req = requisicao
+//res = resposta
+
+server.get("/", (req, res) => {
+    res.send("Cheguei ")
+})
+
 // ligar o servidor
 
 server.listen(3000)

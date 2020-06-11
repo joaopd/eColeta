afc2499b0b6ebac87c45bf4a174668150f2095c4
joaopd <40632469+joaopd@users.noreply.github.com>
2020-06-11 20:19:59 -0300

    Servidor criado

--- src/server.js
+++ src/server.js
@@ -1,12 +1,31 @@
 const express = require("express")
 const server = express()
 
+//Configurar pasta public
+
+server.use(express.static("public"))
+
+
+//utilizando templade nunjucks
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
 //configurar caminhos da aplicação
 //req = requisicao
 //res = resposta
 
 server.get("/", (req, res) => {
-    res.send("Cheguei ")
+   return res.render("index.html")
+})
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
 })
 
 // ligar o servidor
